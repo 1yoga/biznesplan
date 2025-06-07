38d4ece05e17375fe2103d0e561887de10d46159
Anton Kazakevich <ant.kazakevich@yandex.ru>
2025-06-08 06:50:18 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { drizzle } = require('drizzle-orm/node-postgres');
 const { Pool } = require('pg');
-const { plans, orders, documents, sections } = require('./schema');
+const { orders, documents, sections } = require('./schema');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const db = drizzle(pool, { schema: { plans, orders, documents, sections } });
+const db = drizzle(pool, { schema: { orders, documents, sections } });
 
-module.exports = { db, plans, orders, documents, sections };
+module.exports = { db, orders, documents, sections };
